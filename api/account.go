87c1d10b716e4c59ab0ee1b880ed81b6a8bcac28
Simon Juba <scimail09@gmail.com>
2022-10-02 14:09:58 +0900

    add account creation handler

---
+++ api/account.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	db "github.com/smelton01/bank/db/sqlc"
+)
+
+type createAccountRequest struct {
+	Currency string `json:"currency" binding:"required,currency"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	// authPayload := ctx.MustGet(authorizationPayloadKey)//.(*token.Payload)
+	arg := db.CreateAccountParams{
+		// Owner:    authPayload.Username,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}

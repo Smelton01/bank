f8e58f9818011333c665b11cbcb21643dc1ea098
Simon Juba <scimail09@gmail.com>
2022-11-15 23:01:55 +0900

    api: create server

--- api/server.go
+++ api/server.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	db "github.com/smelton01/bank/db/sqlc"
 )
@@ -23,7 +25,18 @@ func NewServer(store *db.Store) *Server {
 func (server *Server) setupRouter() {
 	router := gin.Default()
 
+	router.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, struct {
+			Status string `json:"test"`
+		}{Status: "ok"})
+	})
 	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
 
 	server.router = router
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
 }
